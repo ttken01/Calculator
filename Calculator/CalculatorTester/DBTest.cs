c6e8906190844bdc95cfe1bebbe64bc1bd1f1536
ttken01 <kennguyen.cr1@gmail.com>
2022-03-03 10:32:42 +0700

    Database test

---
+++ Calculator/CalculatorTester/DBTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Data.Schema.UnitTesting;
+using Microsoft.Data.Schema.UnitTesting.Conditions;
+
+namespace CalculatorTester
+{
+    [TestClass()]
+    public class DBTest : DatabaseTestClass
+    {
+
+        public DBTest()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        [TestMethod()]
+        public void DatabaseTest1()
+        {
+            DatabaseTestActions testActions = this.DatabaseTest1Data;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            ExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            // Execute the test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+            ExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            // Execute the post-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+            ExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Schema.UnitTesting.DatabaseTestAction DatabaseTest1_TestAction;
+            Microsoft.Data.Schema.UnitTesting.Conditions.ChecksumCondition checksumCondition1;
+            Microsoft.Data.Schema.UnitTesting.Conditions.EmptyResultSetCondition emptyResultSetCondition1;
+            Microsoft.Data.Schema.UnitTesting.Conditions.ExecutionTimeCondition executionTimeCondition1;
+            Microsoft.Data.Schema.UnitTesting.Conditions.ExpectedSchemaCondition expectedSchemaCondition1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DBTest));
+            Microsoft.Data.Schema.UnitTesting.Conditions.RowCountCondition rowCountCondition1;
+            this.DatabaseTest1Data = new Microsoft.Data.Schema.UnitTesting.DatabaseTestActions();
+            DatabaseTest1_TestAction = new Microsoft.Data.Schema.UnitTesting.DatabaseTestAction();
+            checksumCondition1 = new Microsoft.Data.Schema.UnitTesting.Conditions.ChecksumCondition();
+            emptyResultSetCondition1 = new Microsoft.Data.Schema.UnitTesting.Conditions.EmptyResultSetCondition();
+            executionTimeCondition1 = new Microsoft.Data.Schema.UnitTesting.Conditions.ExecutionTimeCondition();
+            expectedSchemaCondition1 = new Microsoft.Data.Schema.UnitTesting.Conditions.ExpectedSchemaCondition();
+            rowCountCondition1 = new Microsoft.Data.Schema.UnitTesting.Conditions.RowCountCondition();
+            // 
+            // DatabaseTest1Data
+            // 
+            this.DatabaseTest1Data.PosttestAction = null;
+            this.DatabaseTest1Data.PretestAction = null;
+            this.DatabaseTest1Data.TestAction = DatabaseTest1_TestAction;
+            // 
+            // DatabaseTest1_TestAction
+            // 
+            DatabaseTest1_TestAction.Conditions.Add(checksumCondition1);
+            DatabaseTest1_TestAction.Conditions.Add(emptyResultSetCondition1);
+            DatabaseTest1_TestAction.Conditions.Add(executionTimeCondition1);
+            DatabaseTest1_TestAction.Conditions.Add(expectedSchemaCondition1);
+            DatabaseTest1_TestAction.Conditions.Add(rowCountCondition1);
+            resources.ApplyResources(DatabaseTest1_TestAction, "DatabaseTest1_TestAction");
+            // 
+            // checksumCondition1
+            // 
+            checksumCondition1.Checksum = null;
+            checksumCondition1.Enabled = true;
+            checksumCondition1.Name = "checksumCondition1";
+            // 
+            // emptyResultSetCondition1
+            // 
+            emptyResultSetCondition1.Enabled = true;
+            emptyResultSetCondition1.Name = "emptyResultSetCondition1";
+            emptyResultSetCondition1.ResultSet = 1;
+            // 
+            // executionTimeCondition1
+            // 
+            executionTimeCondition1.Enabled = true;
+            executionTimeCondition1.ExecutionTime = System.TimeSpan.Parse("00:00:30");
+            executionTimeCondition1.Name = "executionTimeCondition1";
+            // 
+            // expectedSchemaCondition1
+            // 
+            expectedSchemaCondition1.Enabled = true;
+            expectedSchemaCondition1.Name = "expectedSchemaCondition1";
+            resources.ApplyResources(expectedSchemaCondition1, "expectedSchemaCondition1");
+            expectedSchemaCondition1.Verbose = false;
+            // 
+            // rowCountCondition1
+            // 
+            rowCountCondition1.Enabled = true;
+            rowCountCondition1.Name = "rowCountCondition1";
+            rowCountCondition1.ResultSet = 1;
+            rowCountCondition1.RowCount = 0;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        private DatabaseTestActions DatabaseTest1Data;
+    }
+}
